a7ce24b4e284fe6edcf6eb8dd17abbb828ed11a4
Demo User <ubuntu@arm.localdomain>
2017-09-10 21:08:17 +0800

    playback

--- playback.c
+++ playback.c
@@ -12,27 +12,18 @@ to the default PCM device for 5 seconds of data.
 
 #include <alsa/asoundlib.h>
 
-int main(int argc,char *argv[]) {
-  long loops;
-  int rc;
-  int size;
+int main(int argc,char *argv[])
+{  
   snd_pcm_t *handle;
-  snd_pcm_hw_params_t *params;
-  unsigned int val;
-  int dir;
-  snd_pcm_uframes_t frames;
-  char *buffer;
-  
   /* Open PCM device for playback. */
-  rc = snd_pcm_open(&handle, argv[1], 
-                    SND_PCM_STREAM_PLAYBACK, 0);
-  if (rc < 0) {
-    fprintf(stderr, 
-            "unable to open pcm device: %s\n",
-            snd_strerror(rc));
+  int rc = snd_pcm_open(&handle, argv[1], SND_PCM_STREAM_PLAYBACK, 0);
+  if (rc < 0)
+  {
+    fprintf(stderr, "unable to open pcm device: %s\n",snd_strerror(rc));
     exit(1);
   }
   
+  snd_pcm_hw_params_t *params;
   /* Allocate a hardware parameters object. */
   snd_pcm_hw_params_alloca(&params);
 
@@ -42,70 +33,70 @@ int main(int argc,char *argv[]) {
   /* Set the desired hardware parameters. */
 
   /* Interleaved mode */
-  snd_pcm_hw_params_set_access(handle, params,
-                      SND_PCM_ACCESS_RW_INTERLEAVED);
+  snd_pcm_hw_params_set_access(handle, params,SND_PCM_ACCESS_RW_INTERLEAVED);
 
   /* Signed 16-bit little-endian format */
-  snd_pcm_hw_params_set_format(handle, params,
-                              SND_PCM_FORMAT_S16_LE);
+  snd_pcm_hw_params_set_format(handle, params,SND_PCM_FORMAT_S16_LE);
 
   /* Two channels (stereo) */
   snd_pcm_hw_params_set_channels(handle, params, 1);
 
   /* 44100 bits/second sampling rate (CD quality) */
-  val = 24000;
-  snd_pcm_hw_params_set_rate_near(handle, params, 
-                                  &val, &dir);
+  unsigned int val = 48000;
+  int dir;
+  snd_pcm_hw_params_set_rate_near(handle, params, &val, &dir);
 
   /* Set period size to 32 frames. */
-  frames = 48;
-  snd_pcm_hw_params_set_period_size_near(handle,
-                              params, &frames, &dir);
+  snd_pcm_uframes_t frames = 48;
+  snd_pcm_hw_params_set_period_size_near(handle,params, &frames, &dir);
 
   /* Write the parameters to the driver */
   rc = snd_pcm_hw_params(handle, params);
-  if (rc < 0) {
-    fprintf(stderr, 
-            "unable to set hw parameters: %s\n",
-            snd_strerror(rc));
+  if (rc < 0)
+  {
+    fprintf(stderr, "unable to set hw parameters: %s\n",snd_strerror(rc));
     exit(1);
   }
 
   /* Use a buffer large enough to hold one period */
-  snd_pcm_hw_params_get_period_size(params, &frames,
-                                    &dir);
-  size = frames * 2; /* 2 bytes/sample, 2 channels */
-  buffer = (char *) malloc(size);
+  snd_pcm_hw_params_get_period_size(params, &frames,&dir);
+
+  int size = frames * 2; /* 2 bytes/sample, 2 channels */
+  char *buffer = (char *) malloc(size);
 
   /* We want to loop for 5 seconds */
-  snd_pcm_hw_params_get_period_time(params, 
-                                    &val, &dir);
+  snd_pcm_hw_params_get_period_time(params, &val, &dir);
   /* 5 seconds in microseconds divided by 
    * period time */
-  loops = 5000000 / val;
+  long loops = 5000000 / val;
 
-  while (loops > 0) {
+  while (loops > 0)
+  {
     loops--;
     rc = read(0, buffer, size);
-    if (rc == 0) {
+    if (rc == 0)
+    {
       fprintf(stderr, "end of file on input\n");
       break;
-    } else if (rc != size) {
-      fprintf(stderr,
-              "short read: read %d bytes\n", rc);
+    }
+    else if (rc != size)
+    {
+      fprintf(stderr,"short read: read %d bytes\n", rc);
     }
     rc = snd_pcm_writei(handle, buffer, frames);
-    if (rc == -EPIPE) {
+    if (rc == -EPIPE)
+    {
       /* EPIPE means underrun */
       fprintf(stderr, "underrun occurred\n");
       snd_pcm_prepare(handle);
-    } else if (rc < 0) {
-      fprintf(stderr,
-              "error from writei: %s\n",
-              snd_strerror(rc));
-    }  else if (rc != (int)frames) {
-      fprintf(stderr, 
-              "short write, write %d frames\n", rc);
+    }
+    else if (rc < 0)
+    {
+      fprintf(stderr,"error from writei: %s\n",snd_strerror(rc));
+    }
+    else if (rc != (int)frames)
+    {
+      fprintf(stderr, "short write, write %d frames\n", rc);
     }
   }
 
